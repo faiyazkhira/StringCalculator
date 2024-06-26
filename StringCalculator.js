cae52138bed2ec876ef954efbf717aaf510e86d7
Faiyaz Khira <faiyazkhira27@gmail.com>
2024-06-26 17:45:29 +0530

    add feature to throw error when encountered with negative number(s)

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,16 +2,22 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-    let delimiter = /,|\n/; // Default delimiters
+     let delimiter = /,|\n/; // Default delimiters: comma or newline
     if (numbers.startsWith("//")) {
         const delimiterEndIndex = numbers.indexOf('\n');
         let delimiterStr = numbers.substring(2, delimiterEndIndex);
-        delimiterStr = delimiterStr.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'); // Escape special characters
+        delimiterStr = delimiterStr.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'); // Escape special characters in delimiter
         delimiter = new RegExp(delimiterStr);
         numbers = numbers.substring(delimiterEndIndex + 1);
     }
+
     const numArray = numbers.split(delimiter).map(Number);
-    console.log(`Split numbers: ${numArray}`);
+    const negatives = numArray.filter(num => num < 0);
+
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+    }
+
     return numArray.reduce((sum, num) => sum + num, 0);
     
 }
