ccb178f1b35ae414d937f0cfb22ab09e56c753ad
Rachit  Bansal <rachit@Rachits-Laptop.local>
2024-06-26 16:49:31 +0530

    add feature to handle two numbers and new lines between numbers

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -4,8 +4,20 @@ test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
 
+//haandling single number
 test('should return the number itself for a single number string', () => {
     expect(add("1")).toBe(1);
     expect(add("5")).toBe(5);
+});
+
+//handling two numbers
+test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("3,5")).toBe(8);
+});
+
+//Handling New Lines Between Numbers
+test('should handle new lines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
 });
 
