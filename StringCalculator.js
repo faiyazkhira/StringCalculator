4e5efbbc0773f13a291c472c8cc3eb9a028c4071
Rachit  Bansal <rachit@Rachits-Laptop.local>
2024-06-26 17:38:00 +0530

    add feature to handle different delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,8 +2,18 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-    const numArray = numbers.split(/[\n,]/);
-    return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
+    let delimiter = /,|\n/; // Default delimiters
+    if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf('\n');
+        let delimiterStr = numbers.substring(2, delimiterEndIndex);
+        delimiterStr = delimiterStr.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'); // Escape special characters
+        delimiter = new RegExp(delimiterStr);
+        numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+    const numArray = numbers.split(delimiter).map(Number);
+    console.log(`Split numbers: ${numArray}`);
+    return numArray.reduce((sum, num) => sum + num, 0);
+    
 }
 
 module.exports = add;
